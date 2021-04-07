e6a7872de511436e7a20f6ecdec3c1f1d4d15336
RiLerIE <73770014+RiLerIE@users.noreply.github.com>
2021-04-07 22:55:16 +0800

    Add files via upload

---
+++ print1.py
@@ -0,0 +1,18 @@
+print('Hello World')
+print('Hey,Where are you going?')
+print('Peter','James','Lee')
+print('Peter James Lee')
+print(1+1)
+print(1+5)
+#1+1 不能這樣直接輸入，需鍵入print才能將內容數字相加。
+#print(_) 錯誤輸入語法。
+#_ 錯誤輸入語法。
+print('_')
+print('Peter')
+#print(_) 錯誤輸入語法。
+print('_')
+print('Hello world','Hey','Good')
+print('Hello world',',','Hey',',','Good')
+print('Peter' , ',' , 'James' , ',' , 'John')
+print('Peter','James','John')
+print('Peter',',','James',',','John')
